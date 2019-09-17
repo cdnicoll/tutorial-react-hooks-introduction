ae5a129bf9d2f914794dce2b18d5678995345e30
Cody Nicoll <cdnicoll@gmail.com>
2019-09-17 12:44:44 -0700

    ⭐️ added hooks to the char picker, also used the useEffect hook in place of componentDidMount

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './CharPicker.css';
 
-class CharPicker extends Component {
-  state = { characters: [], isLoading: false };
+const CharPicker = props => {
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  componentDidMount() {
-    this.setState({ isLoading: true });
+  useEffect(() => {
+    setIsLoading(true);
     fetch('https://swapi.co/api/people')
       .then(response => {
         if (!response.ok) {
@@ -16,48 +17,42 @@ class CharPicker extends Component {
       })
       .then(charData => {
         const selectedCharacters = charData.results.slice(0, 5);
-        this.setState({
-          characters: selectedCharacters.map((char, index) => ({
+
+        setCharacters(
+          selectedCharacters.map((char, index) => ({
             name: char.name,
-            id: index + 1
+            id: index + 1,
           })),
-          isLoading: false
-        });
+        );
+        setIsLoading(false);
       })
       .catch(err => {
         console.log(err);
       });
-  }
+  }, []); // Note that when using an empty array, this behaves as componentDidMount
+          // if I didn't use an empty array, the useEffect method would be rendered
+          // twice because the state property changed, causing an infinite loop
 
-  render() {
-    let content = <p>Loading characters...</p>;
+  let content = <p>Loading characters...</p>;
 
-    if (
-      !this.state.isLoading &&
-      this.state.characters &&
-      this.state.characters.length > 0
-    ) {
-      content = (
-        <select
-          onChange={this.props.onCharSelect}
-          value={this.props.selectedChar}
-          className={this.props.side}
-        >
-          {this.state.characters.map(char => (
-            <option key={char.id} value={char.id}>
-              {char.name}
-            </option>
-          ))}
-        </select>
-      );
-    } else if (
-      !this.state.isLoading &&
-      (!this.state.characters || this.state.characters.length === 0)
-    ) {
-      content = <p>Could not fetch any data.</p>;
-    }
-    return content;
+  if (!isLoading && characters && characters.length > 0) {
+    content = (
+      <select
+        onChange={props.onCharSelect}
+        value={props.selectedChar}
+        className={props.side}
+      >
+        {characters.map(char => (
+          <option key={char.id} value={char.id}>
+            {char.name}
+          </option>
+        ))}
+      </select>
+    );
+  } else if (!isLoading && (!characters || characters.length === 0)) {
+    content = <p>Could not fetch any data.</p>;
   }
-}
+  return content;
+};
 
 export default CharPicker;
